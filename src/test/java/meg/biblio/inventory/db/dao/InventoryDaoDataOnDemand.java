a950ddf38b3a56ea3a1b132d3fd9c4bf12a281a6
Roo Git Add-On <s2-roo@vmware.com>
2015-10-14 23:05:17 +0200

    Revert "new: working on checkout mystery."
    This reverts commit 4fb70f0f3cc373c3a7b3931973605239cf3f2aaf.

--- src/test/java/meg/biblio/inventory/db/dao/InventoryDaoDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package meg.biblio.inventory.db.dao;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = InventoryDao.class)
-public class InventoryDaoDataOnDemand {
-}
