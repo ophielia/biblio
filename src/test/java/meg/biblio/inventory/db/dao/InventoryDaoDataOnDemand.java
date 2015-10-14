4fb70f0f3cc373c3a7b3931973605239cf3f2aaf
Roo Git Add-On <s2-roo@vmware.com>
2015-10-14 22:39:58 +0200

    new: working on checkout mystery.

---
+++ src/test/java/meg/biblio/inventory/db/dao/InventoryDaoDataOnDemand.java
@@ -0,0 +1,6 @@
+package meg.biblio.inventory.db.dao;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = InventoryDao.class)
+public class InventoryDaoDataOnDemand {
+}
